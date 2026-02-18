fffb15417f292b238327a4771b24873094451b4d
WVandex <wolfvandex@gmail.com>
2026-02-18 11:20:16 +0100

    chore: Add frontend URL to cors.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Enable Cross-Origin Resource Sharing
-CORS(app)
+# Enable CORS for a specific origin
+CORS(app, resources={r"/*": {"origins": "https://csc-310-group-5.vercel.app"}})
 
-# Import routes after app initialization to prevent circular imports
+# Import routes after app initialization
 from app import routes
