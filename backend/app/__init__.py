edad82ddb12ef7f3e451f1643ce702a2de1f24c5
WVandex <wolfvandex@gmail.com>
2026-02-25 10:02:48 +0100

    worked on json

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # Enable CORS for a specific origin
-CORS(app, resources={r"/*": {"origins": "https://csc-310-group-5.vercel.app"}})
+CORS(app)
 
 # Import routes after app initialization
 from app import routes
